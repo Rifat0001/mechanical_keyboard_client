8d984809fa367b2bf99886f8052bb53f28353090
Rifat Mahmud <mahmudrifat764@gmail.com>
2024-07-28 02:35:17 +0600

    ui done

--- src/components/ui/ProgressBar.tsx
+++ src/components/ui/ProgressBar.tsx
@@ -1,7 +1,7 @@
 const ProgressBar = () => {
   return (
-    <div className="my-8 flex mt-16 justify-center">
-      <span className="loading loading-spinner text-violet-500 w-[100px] lg:w-[200px] "></span>
+    <div className="my-8 flex mt-20 justify-center">
+      <span className="loading loading-spinner  text-violet-300 w-[100px] lg:w-[200px] "></span>
     </div>
   );
 };
